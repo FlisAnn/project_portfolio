1f00fe4b8fe92eef20ec6059d0bb728cb31c370d
flis_ann <ann.flismark@gmail.com>
2020-12-22 20:29:35 +0100

    Adding project information to json file

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React from "react";
 import { Card, Image } from "semantic-ui-css";
 
 const ProjectCard = ({ project }) => {
   return (
-    <Card>
-      <Image src={project.image} wrapped ui={false} />
-      <Card.Content>
-        <Card.Header>{project.name}</Card.Header>
-        <Card.Description>{project.description}</Card.Description>
-      </Card.Content>
-    </Card>
-  )
-}
+    <Card.Group>
+      <Card>
+        <Image src={project.image} wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>{project.name}</Card.Header>
+          <Card.Meta>{project.extra}</Card.Meta>
+          <Card.Description>{project.description}</Card.Description>
+        </Card.Content>
+        <Card.Content>
+          <a href={project.url}>
+            <Icon name="external" id={`urlIcon-${project.id}`} />
+          </a>
+          <a href={project.github}>
+            <Icon name="github square" id={`gitIcon-${project.id}`} />
+          </a>
+        </Card.Content>
+      </Card>
+    </Card.Group>
+  );
+};
 
 export default ProjectCard;
