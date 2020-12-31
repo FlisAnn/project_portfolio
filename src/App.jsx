5f9d5fab3a5d2326ec0a89337430e7159e615e62
flis_ann <ann.flismark@gmail.com>
2020-12-31 22:38:35 +0100

    Fixing footer, renaming about, adding text to hello component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Hello from './components/Hello'
-import About from "./components/About";
+import Resume from "./components/Resume";
 import Projects from "./components/Projects";
 import { Switch, Route } from "react-router-dom";
 
@@ -12,7 +12,7 @@ const App = () => {
       <Header />
       <Switch>
         <Route exact path="/" component={Hello}></Route>
-        <Route exact path="/resume" component={About}></Route>
+        <Route exact path="/resume" component={Resume}></Route>
         <Route exact path="/projects" component={Projects}></Route>
         <Hello />
       </Switch>
--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { Container } from "semantic-ui-react";
+import { Container, Segment } from "semantic-ui-react";
 
 const Hello = () => {
   return (
     <Container>
+      <Segment raised>
       <h1 data-cy="hello">Hello World</h1>
+      <p>Welcome to my place on the net. Here you can find my resume as well as the projects I've created during my boot camp at Craft Academy.</p>
+      </Segment>
     </Container>
   );
 };
