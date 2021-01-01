f68c6970d4eedca4d10c1493118ebe016fc973d8
flis_ann <ann.flismark@gmail.com>
2021-01-01 16:26:35 +0100

    Refining Portfolio images

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -6,7 +6,7 @@ const Hello = () => {
     <Container>
       <Segment raised>
       <h1 data-cy="hello">Welcome</h1>
-      <p>Welcome to my place on the net. Here you can find my resume as well as the projects I've created during my boot camp at Craft Academy.</p>
+      <p>My name is Ann Flismark and this is </p>
       </Segment>
     </Container>
   );
