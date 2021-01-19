7dd9cb2d3d0d1d113467d2a40a1b5e6718e59bc4
flis_ann <ann.flismark@gmail.com>
2021-01-19 21:15:24 +0100

    Rearrangeing the json file to show the latest project first, adding space between icons and contact information

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -5,28 +5,87 @@ const About = () => {
   return (
     <Container>
       <h1 data-cy="resume-header">My Resume</h1>
-      <Divider horizontal>Contact Information</Divider>
+      <Divider horizontal >Contact Information</Divider>
       <Item.Group divided relaxed>
         <Item>
           <Item.Content>
             <Item.Meta>
               <a href="https://www.linkedin.com/in/annflismark/">
-                <Icon circular color="blue" name="linkedin alternate" />
+                <Icon circular color="blue" name="linkedin alternate" 
+                style={{
+                  marginRight: 10,
+                }}
+                  />
                 Ann Flismark
               </a>
             </Item.Meta>
           </Item.Content>
           <Item.Content>
             <Item.Meta>
-              <Icon circular color="blue" name="envelope" />
+              <Icon circular color="blue" name="envelope" 
+              style={{
+                marginRight: 10,
+              }}
+              />
               ann.flismark@gmail.com
             </Item.Meta>
           </Item.Content>
           <Item.Content>
             <Item.Meta>
-              <Icon circular color="blue" name="mobile alternate" />
+              <Icon circular color="blue" name="mobile alternate" 
+              style={{
+                marginRight: 10,
+              }}
+              />
               +46 762 350 512
             </Item.Meta>
+          </Item.Content>
+        </Item>
+      </Item.Group>
+
+      <Divider horizontal >Education</Divider>
+
+      <Item.Group divided>
+        <Item>
+          <Item.Image size="tiny" src="./images/craft.jpeg" />
+
+          <Item.Content>
+            <Item.Header as="a">Craft Academy</Item.Header>
+            <Item.Meta>
+              <span>October 2020 - February 2021</span>
+            </Item.Meta>
+            <Item.Description>
+              12 week boot camp to become a Full Stack Developer. 
+            </Item.Description>
+            <Item.Extra>
+              <Label>React</Label>
+              <Label>Ruby on Rails</Label>
+              <Label>NodeJS</Label>
+              <Label>Cypress</Label>
+              <Label>RSpec</Label>
+              <Label>git</Label>
+              <Label>Pair programming</Label>
+              <Label>Scrum</Label>
+            </Item.Extra>
+          </Item.Content>
+        </Item>
+
+        <Item>
+          <Item.Image size="tiny" src="./images/sh.jpeg" />
+          <Item.Content>
+            <Item.Header as="a">Södertörns Högskola</Item.Header>
+            <Item.Meta>
+              <span>2004 - 2007</span>
+            </Item.Meta>
+            <Item.Description>
+              Bachelor Degree in IT, Media & Design
+            </Item.Description>
+            <Item.Extra>
+              <Label>PHP</Label>
+              <Label>Java</Label>
+              <Label>Database</Label>
+              <Label>Project Management</Label>
+            </Item.Extra>
           </Item.Content>
         </Item>
       </Item.Group>
@@ -103,52 +162,6 @@ const About = () => {
               <Label>Scrum</Label>
               <Label>Kanban</Label>
               <Label>Scrum Master</Label>
-            </Item.Extra>
-          </Item.Content>
-        </Item>
-      </Item.Group>
-
-      <Divider horizontal>Education</Divider>
-
-      <Item.Group divided>
-        <Item>
-          <Item.Image size="tiny" src="./images/craft.jpeg" />
-
-          <Item.Content>
-            <Item.Header as="a">Craft Academy</Item.Header>
-            <Item.Meta>
-              <span>October 2020 - February 2021</span>
-            </Item.Meta>
-            <Item.Description>
-              12 week boot camp to become a Full Stack Developer
-            </Item.Description>
-            <Item.Extra>
-              <Label>React</Label>
-              <Label>Ruby on Rails</Label>
-              <Label>Cypress</Label>
-              <Label>RSpec</Label>
-              <Label>Scrum</Label>
-              <Label>GitHub</Label>
-              <Label>Pair programming</Label>
-            </Item.Extra>
-          </Item.Content>
-        </Item>
-
-        <Item>
-          <Item.Image size="tiny" src="./images/sh.jpeg" />
-          <Item.Content>
-            <Item.Header as="a">Södertörns Högskola</Item.Header>
-            <Item.Meta>
-              <span>2004 - 2007</span>
-            </Item.Meta>
-            <Item.Description>
-              Bachelor Degree in IT, Media & Design
-            </Item.Description>
-            <Item.Extra>
-              <Label>PHP</Label>
-              <Label>Java</Label>
-              <Label>Database</Label>
-              <Label>Project Management</Label>
             </Item.Extra>
           </Item.Content>
         </Item>
